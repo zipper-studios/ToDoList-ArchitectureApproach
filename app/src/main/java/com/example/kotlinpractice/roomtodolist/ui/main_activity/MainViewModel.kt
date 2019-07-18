2df5553306cff8832ee075973251acf0ec6c0418
Vlad Negrea <vlad@zipperstudios.co>
2019-07-18 16:05:53 +0300

    Implemented force refresh for todos

--- app/src/main/java/com/example/kotlinpractice/roomtodolist/ui/main_activity/MainViewModel.kt
+++ app/src/main/java/com/example/kotlinpractice/roomtodolist/ui/main_activity/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinpractice.roomtodolist.ui.main_activity
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
 import com.example.kotlinpractice.roomtodolist.model.Task
 import com.example.kotlinpractice.roomtodolist.persistance.Repository
@@ -8,14 +10,16 @@ import com.example.kotlinpractice.roomtodolist.utils.Resource
 
 class MainViewModel(val repository: Repository) : ViewModel() {
 
+    private val _refreshItems = MutableLiveData<Unit>()
+
     val mainAdapter = MainAdapter()
-    var itemsList: LiveData<Resource<List<Task>>> = repository.loadRepos()
+    var itemsList: LiveData<Resource<List<Task>>> = Transformations.switchMap(_refreshItems) { repository.loadRepos() }
 
     fun updateList(it: List<Task>) {
         mainAdapter.updateList(it)
     }
 
-    fun onResume(){
-        itemsList = repository.loadRepos()
+    fun onResume() {
+        _refreshItems.value = Unit
     }
 }
